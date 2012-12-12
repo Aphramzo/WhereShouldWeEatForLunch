cd80cc50ad69715d76c48e6cff3ae708ddd6cf8b
Aphramzo <im.t.wal@gmail.com>
2012-12-12 13:28:23 -0700

    First basic search screen return result

--- WhereShouldWeEatLunch/Controllers/SearchController.cs
+++ WhereShouldWeEatLunch/Controllers/SearchController.cs
@@ -3,34 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WhereShouldWeEatLunch.Models;
 
 namespace WhereShouldWeEatLunch.Controllers
 {
     public class SearchViewModel
     {
         public Boolean IsWalkingDistance { get; set; }
+        public List<EateryModel> Eateries { get; set; }
+        public EateryModel SuggestedEatery { get; set; }
     }
     public class SearchController : Controller
     {
         //
         // GET: /Search/
-
+        private WhereShouldWeEatLunchContext db = new WhereShouldWeEatLunchContext();
         public ActionResult Index(FormCollection values)
         {
-            SearchViewModel viewModel;
+            var viewModel = new SearchViewModel();
+            var eateries = new List<EateryModel>();
+            viewModel.Eateries = eateries;
             if (values.GetValues("IsWalkingDistance") != null)
             {
-                viewModel = new SearchViewModel()
-                                {
-                                    IsWalkingDistance = values.GetValues("IsWalkingDistance").Contains("true")
-                                };
+                var isWalkingDistance = values.GetValues("IsWalkingDistance").Contains("true");
+                viewModel.IsWalkingDistance = isWalkingDistance;
+                viewModel.Eateries =
+                    db.EateryModels.Where(x => x.IsWalkingDistance == isWalkingDistance).OrderBy(c => c.Name).ToList();
+                viewModel.SuggestedEatery = GetSuggestionFromList(viewModel.Eateries);
             }
-            else
-                viewModel = new SearchViewModel();
             
             return View(viewModel);
         }
-        
-       
+
+        private EateryModel GetSuggestionFromList(List<EateryModel> eateries)
+        {
+            var rand = new Random();
+            return eateries.ElementAt(rand.Next(0, eateries.Count));
+        }
     }
 }
