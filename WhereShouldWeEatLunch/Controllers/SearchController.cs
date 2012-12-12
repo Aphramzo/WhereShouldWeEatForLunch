c2ad392130f7639d2d9e669e886c16d7361ea541
Aphramzo <im.t.wal@gmail.com>
2012-12-12 13:16:52 -0700

    Hmmm this doesnt seem like the right way to build forms...

---
+++ WhereShouldWeEatLunch/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WhereShouldWeEatLunch.Controllers
+{
+    public class SearchViewModel
+    {
+        public Boolean IsWalkingDistance { get; set; }
+    }
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+
+        public ActionResult Index(FormCollection values)
+        {
+            SearchViewModel viewModel;
+            if (values.GetValues("IsWalkingDistance") != null)
+            {
+                viewModel = new SearchViewModel()
+                                {
+                                    IsWalkingDistance = values.GetValues("IsWalkingDistance").Contains("true")
+                                };
+            }
+            else
+                viewModel = new SearchViewModel();
+            
+            return View(viewModel);
+        }
+        
+       
+    }
+}
--- WhereShouldWeEatLunch/Global.asax.cs
+++ WhereShouldWeEatLunch/Global.asax.cs
@@ -24,7 +24,7 @@ namespace WhereShouldWeEatLunch
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Search", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
