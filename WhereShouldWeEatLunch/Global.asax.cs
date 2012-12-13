c52957e9a125d2c40e0238666456ee99aa01096e
Aphramzo <im.t.wal@gmail.com>
2012-12-13 15:30:40 -0700

    Changed home route to the foursquare search page

--- WhereShouldWeEatLunch/Global.asax.cs
+++ WhereShouldWeEatLunch/Global.asax.cs
@@ -24,7 +24,7 @@ namespace WhereShouldWeEatLunch
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Search", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Eatery", action = "FourSquareList", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
