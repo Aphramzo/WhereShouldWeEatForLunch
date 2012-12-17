c94771e017bf153dfe977e13eb569f2076eaccb5
Aphramzo <im.t.wal@gmail.com>
2012-12-17 11:59:18 -0700

    Start on Yelp integration

---
+++ WhereShouldWeEatLunch/APIs/Yelp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web;
+using OAuth;
+using RestSharp;
+using YelpSharp;
+using YelpSharp.Data.Options;
+
+namespace WhereShouldWeEatLunch.APIs
+{
+    public class Yelps : RESTfulAPI
+    {
+        private const String _ConsumerKey = "GET YOUR OWN";
+        private const String _ConsumerSecret = "o8dFWf-GET YOUR OWN";
+        private const String _Token = "GET YOUR OWN";
+        private const String _TokenSecret = "GET YOUR OWN";
+        
+        public void SimpleTest()
+        {
+
+            string _term = "food";
+            string _location = "Denver";
+
+            var y = new Yelp(new Options()
+                        {
+                            ConsumerKey = _ConsumerKey,
+                            ConsumerSecret = _ConsumerSecret,
+                            AccessToken = _Token,
+                            AccessTokenSecret = _TokenSecret
+                        });
+
+            var searchOptions = new SearchOptions();
+            searchOptions.GeneralOptions = new GeneralOptions()
+            {
+                term = _term
+            };
+
+            searchOptions.LocationOptions = new LocationOptions()
+            {
+                location = _location
+            };
+
+
+            var results = y.Search(searchOptions);
+        }
+    }
+}
