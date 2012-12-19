aeb8225d6b995b0d143a204df9906fc350a9546c
Aphramzo <im.t.wal@gmail.com>
2012-12-19 15:56:35 -0700

    Putting back in desktop version

--- WhereShouldWeEatLunch/Scripts/FourSquareDetail.js
+++ WhereShouldWeEatLunch/Scripts/FourSquareDetail.js
@@ -1,5 +1,7 @@
 ﻿$(document).ready(function () {
-    window.mySwipe = new Swipe(
-                    document.getElementById('slider')
-                );
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+        window.mySwipe = new Swipe(
+            document.getElementById('slider')
+        );
+    }
 });
