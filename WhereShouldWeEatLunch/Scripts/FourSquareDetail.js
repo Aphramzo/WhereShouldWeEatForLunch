546a84b9fe473efb8f8df67e6252aa36ff0c6b97
Aphramzo <im.t.wal@gmail.com>
2012-12-20 09:09:03 -0700

    removed alert

--- WhereShouldWeEatLunch/Scripts/FourSquareDetail.js
+++ WhereShouldWeEatLunch/Scripts/FourSquareDetail.js
@@ -1,6 +1,5 @@
 ﻿$(document).ready(function () {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-        alert('loading swipe');
         window.mySwipe = new Swipe(
             document.getElementById('slider')
         );
