67ffdbed98c095db3a7d269ec4bc399b9818daca
unknown <mvondrak@DENLT-MVONDRAK1.ExperianConsumerDirect.com>
2013-02-28 15:54:01 -0700

    added color test pages

---
+++ WhereShouldWeEatLunch/Controllers/ColorTestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WhereShouldWeEatLunch.Controllers
+{
+    public class ColorTestController : Controller
+    {
+        //
+        // GET: /ColorTest/
+
+        public ActionResult ColorTest1()
+        {
+            return View();
+        }
+        public ActionResult ColorTest2()
+        {
+            return View();
+        }
+
+    }
+}
