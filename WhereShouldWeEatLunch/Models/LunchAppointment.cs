276a890e7436de31899051f7898a54719f939523
Aphramzo <im.t.wal@gmail.com>
2013-03-01 15:29:10 -0700

    Getting missing files

---
+++ WhereShouldWeEatLunch/Models/LunchAppointment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhereShouldWeEatLunch.Models
+{
+    public class LunchAppointment
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public String FourSquareId { get; set; }
+
+        public virtual UserModel UserModel { get; set; }
+    }
+}
